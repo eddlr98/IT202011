fc2837ed98238a5b1e7aecf0935323e13a63fe93
eddlr98 <eddlr98@gmail.com>
2020-11-19 15:46:10 -0800

    fixed pathing issue

--- project/list_cart.php
+++ project/list_cart.php
@@ -1,4 +1,4 @@
-<?php require_once(__DIR__ . "../partials/nav.php"); ?>
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
 <?php
 if (!has_role("Admin")) {
     //this will redirect to login and kill the rest of this script (prevent it from executing)
@@ -61,4 +61,4 @@ if (isset($_POST["search"]) && !empty($query)) {
         <p>No results</p>
     <?php endif; ?>
 </div>
-<?php require(__DIR__ . "../partials/flash.php");
+<?php require(__DIR__ . "/partials/flash.php");
