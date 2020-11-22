09514ea69bd865704aa83be41ac050dad631b96d
eddlr98 <eddlr98@gmail.com>
2020-11-22 19:17:11 +0000

    added rudimentary bootstrap to be worked on

--- project/list_products_admin.php
+++ project/list_products_admin.php
@@ -46,8 +46,8 @@ if (isset($_POST["search"]) && !empty($query)) {
                         <div><?php safer_echo($r["user_id"]); ?></div>
                     </div>
                     <div>
-                        <a type="button" href="test_edit_products.php?id=<?php safer_echo($r['id']); ?>">Edit</a>
-                        <a type="button" href="test_view_products.php?id=<?php safer_echo($r['id']); ?>">View</a>
+                        <a type="button" href="edit_products_admin.php?id=<?php safer_echo($r['id']); ?>">Edit</a>
+                        <a type="button" href="view_products_admin.php?id=<?php safer_echo($r['id']); ?>">View</a>
                     </div>
                 </div>
             <?php endforeach; ?>
--- project/login.php
+++ project/login.php
@@ -1,12 +1,17 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
     <form method="POST">
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email"/>
-        <label for="username">Username:</label>
-        <input type="username" id="username" name="username"/>
-        <label for="p1">Password:</label>
-        <input type="password" id="p1" name="password" required/>
-        <input type="submit" name="login" value="Login"/>
+        <div class="form-group">
+            <label for="email">Email:</label>
+            <input class="form-control" type="email" id="email" name="email" required/>
+        </div>
+        <div class="form-group">
+            <label for="username">Username:</label>
+            <input class="form-control" type="username" id="username" name="username"/>
+        <div class="form-group">
+            <label for="p1">Password:</label>
+            <input class="form-control" type="password" id="p1" name="password" required/>
+        </div>
+        <input class="btn btn-primary" type="submit" name="login" value="Login"/>
     </form>
 
 <?php
