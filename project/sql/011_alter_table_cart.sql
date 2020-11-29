e6cb4ad516673b74cc0824b09544346ce883a5dc
eddlr98 <eddlr98@gmail.com>
2020-11-29 21:49:25 +0000

    minor edit to alter cart sql

--- project/sql/011_alter_table_cart.sql
+++ project/sql/011_alter_table_cart.sql
@@ -1,4 +1,5 @@
 ALTER TABLE Cart
+    ALTER COLUMN quantity int default 1,
     ALTER COLUMN price      decimal(12, 2) default 999999.99,
     ALTER COLUMN modified   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP on update current_timestamp,
     ALTER COLUMN created    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
