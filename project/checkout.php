0102570fc8c4ff59c88c0e84ddd8301b4a35433c
eddlr98 <eddlr98@gmail.com>
2020-12-10 03:00:03 +0000

    added query

--- project/checkout.php
+++ project/checkout.php
@@ -10,6 +10,17 @@ if (!is_logged_in()) {
 
 
 
+<?php
+$id = get_user_id();
+$db = getDB();
+$stmt = $db->prepare("SELECT cart.id, cart.product_id, cart.quantity, cart.price, Product.name as product FROM Cart as cart JOIN Users on cart.user_id = Users.id LEFT JOIN Products Product on Product.id = cart.product_id where cart.user_id = :id ORDER by product");
+$r = $stmt->execute([":id" => $id]);
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+
+
+
 <div class="card" style="width: 30rem; float: right;">
   <div class="card-body">
     <button style= "margin: 0; float: right;" type="submit" class="btn btn-success">Place your order</button>
@@ -26,15 +37,15 @@ if (!is_logged_in()) {
 <h4>1 Shipping Address</h4>
 
     <div class="input-group mb-3" style="width: 30rem;">
-        <input type="text" name="streetLine1" class="form-control" placeholder="Street Line 1" >
+        <input type="text" name="addrLine1" class="form-control" placeholder="Street address, P.O. box, company name, c/o" required>
     </div>
     <div class="input-group mb-3" style="width: 30rem;">
-        <input type="text" name="streetLine2" class="form-control" placeholder="Street Line 2" >
+        <input type="text" name="addrLine2" class="form-control" placeholder="Apartment, suite, unit, building, floor, etc." >
     </div>
     <div class="input-group mb-3" style="width: 30.6rem;">
-        <input type="text" name="city" class="form-control" placeholder="City" >
-        <input type="text" name="state" class="form-control" placeholder="State" >
-        <input type="text" name="zipCode" class="form-control" placeholder="Zip Code" >
+        <input type="text" name="city" class="form-control" placeholder="City" required>
+        <input type="text" name="state" class="form-control" placeholder="State" required>
+        <input type="text" name="zipCode" class="form-control" placeholder="Zip Code" pattern="[0-9]{5}" required>
     </div>
 
     
@@ -47,32 +58,32 @@ if (!is_logged_in()) {
 
 <div class="form-group">
 		<div class="form-check">
-			<input class="form-check-input" type="radio" name="visibility" id="visibility" value="1">
-			<label class="form-check-label" for="visibility">
+			<input class="form-check-input" type="radio" name="payMethod" id="cash" value="1" checked>
+			<label class="form-check-label" for="payMethod1">
 				Cash
 			</label>
 		</div>
 		<div class="form-check">
-		<input class="form-check-input" type="radio" name="visibility" id="visibility" value="0" checked>
-			<label class="form-check-label" for="visibility">
+		<input class="form-check-input" type="radio" name="payMethod" id="amex" value="2" >
+			<label class="form-check-label" for="payMethod2">
 				American Express
 			</label>
         </div>
         <div class="form-check">
-		<input class="form-check-input" type="radio" name="visibility" id="visibility" value="0" checked>
-			<label class="form-check-label" for="visibility">
+		<input class="form-check-input" type="radio" name="payMethod" id="visa" value="3" >
+			<label class="form-check-label" for="payMethod3">
 				Visa
 			</label>
         </div>
         <div class="form-check">
-		<input class="form-check-input" type="radio" name="visibility" id="visibility" value="0" checked>
-			<label class="form-check-label" for="visibility">
+		<input class="form-check-input" type="radio" name="payMethod" id="mstrcard" value="4" >
+			<label class="form-check-label" for="payMethod4">
 				MasterCard
 			</label>
         </div>
         <div class="form-check">
-		<input class="form-check-input" type="radio" name="visibility" id="visibility" value="0" checked>
-			<label class="form-check-label" for="visibility">
+		<input class="form-check-input" type="radio" name="payMethod" id="paypal" value="5" >
+			<label class="form-check-label" for="payMethod5">
 				Paypal
 			</label>
 		</div>
@@ -94,6 +105,11 @@ if (!is_logged_in()) {
   <input type="text" class="form-control" aria-label="Card Amount">
 </div>
 
-<button style= "margin: 0; float: left;" type="submit" class="btn btn-success">Place your order</button>
+<div class="card" style="width: 30rem; float: left;">
+  <div class="card-header">Order Total:</div>
+  <div class="card-body">
+    <button style= "margin: 0; float: left;" type="submit" class="btn btn-success">Place your order</button>
+  </div>
+</div>
 
 <?php require(__DIR__ . "/partials/flash.php"); ?>
